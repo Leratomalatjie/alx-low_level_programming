a3de206e59e8e3a214112d714e59b7d711e577f2
Leratomaltjie <malatjielt@outlokk.com>
2023-02-16 18:47:30 -0800

    intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.n>
+/**
+ * main - Entry point
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	char b;
+	int c;
+	long int d;
+	long long int e;
+	float f;
+printf("Size of a char: %lu byte(s)\n",(unsigned long)
+sizeof(b));
+printf("Size of an int: %lu byte(s)\n",(unsigned long)
+sizeof(c));
+printf("Size of a long int: %lu byte(s)\n",(unsigned long)
+sizeof(d));
+printf("Size of a long long: %lu byte(s)\n",(unsigned long)
+sizeof(e));
+printf("Size of a float: %lu byte(s)\n",(unsigned long)
+sizeof(f));
+return (0);
+}
