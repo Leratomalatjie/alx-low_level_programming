e881e3c35fc81cae1c281a4acb8634025c74a94b
Leratomalatjie <malatjielt@outlook.com>
2023-06-16 09:27:59 -0700

    lowercase alphabets

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,24 @@
-#include "main.h"
-/**
- * main - print _putchar
- * 
- * Return: 0 (success)
- */
+#include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: This function prints _putchar, followed by a new line.
+ *
+ * Return:0 (Success)
+ */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
 
